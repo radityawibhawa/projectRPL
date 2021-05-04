455b12913e92f9c1a910e8ecbf5cc566fb840f2b
Adinda Rahma Khumairah <60496594+Adinda18@users.noreply.github.com>
2021-05-04 11:53:34 +0700

    Add files via upload
    sudah di rapihin

---
+++ amigo/application/views/pelanggan/footer_pelanggan.php
@@ -0,0 +1,68 @@
+<!-- Bootstrap core JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets/bootstrap/js/jquery-1.10.2.js" type="text/javascript"></script>
+<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript"></script>
+<!-- <script src="assets/js/bayar-konfirmasi.js" type="text/javascript"></script> -->
+<!-- Third party plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+<!-- Contact form JS-->
+<script src="assets/mail/jqBootstrapValidation.js"></script>
+<script src="assets/mail/contact_me.js"></script>
+<!-- Core theme JS-->
+<script src="assets/bootstrap/js/scripts.js"></script>
+<script src="assets/bootstrap/js/bayar-konfirmasi.js"></script>
+
+<!-- Contact-->
+<section class="page-section" id="contact">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Kontak Kami</h2>
+        </div>
+        <form id="contactForm" name="sentMessage" novalidate="novalidate">
+            <div class="row align-items-stretch mb-5">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <input class="form-control" id="name" type="text" placeholder="Masukkan Nama Anda..." required="required" data-validation-required-message="Nama Anda Harus Diisi!" />
+                        <p class="help-block text-danger"></p>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" id="email" type="email" placeholder="Masukkan Email Anda.." required="required" data-validation-required-message="Email Anda Harus Diisi!" />
+                        <p class="help-block text-danger"></p>
+                    </div>
+                    <div class="form-group mb-md-0">
+                        <input class="form-control" id="phone" type="tel" placeholder="Masukkan Nomor Telepon Anda.." required="required" data-validation-required-message="Nomor Telepon Anda Harus Diisi!" />
+                        <p class="help-block text-danger"></p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group form-group-textarea mb-md-0">
+                        <textarea class="form-control" id="message" placeholder="Tulis Pesanmu Disini" required="required" data-validation-required-message="Pesan Anda Harus Terisi!"></textarea>
+                        <p class="help-block text-danger"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center">
+                <div id="success"></div>
+                <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Kirim Pesan</button>
+            </div>
+        </form>
+    </div>
+</section>
+<!-- Footer-->
+<footer class="footer py-4">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-4 text-lg-left">Copyright © Telkom/AmigoticsRent.com</div>
+            <div class="col-lg-4 my-3 my-lg-0">
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="col-lg-4 text-lg-right">
+                <a class="mr-3" href="#!">Syarat Ketentuan</a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ amigo/application/views/pelanggan/header_pelanggan.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Amigotics Rent</title>
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Poppins" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='https://use.fontawesome.com/releases/v5.8.1/css/all.css' rel='stylesheet'>
+    <link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet" />
+    <link href="../../../assets/bootstrap/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet" />
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/styles.css'); ?>" />
+
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/logos.png" alt="" /></a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars ml-1"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ml-auto">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#beranda">Beranda</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#tentangkami">Tentang Kami</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#sewa">Sewa Mobil</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#pengembalian">Pengembalian</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#profil">Profil Anda</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
