d4098fdd0752ca1cffe03c2737aa0f976f770c06
rifaldositepu <62953043+rifaldositepu@users.noreply.github.com>
2021-05-25 15:18:34 +0700

    Update pengembalian.php

--- amigo/application/views/pelanggan/pengembalian.php
+++ amigo/application/views/pelanggan/pengembalian.php
@@ -6,8 +6,8 @@
     </div>
 </header>
 
-<!-- daftar mobil-->
-<section class="page-section" id="pengembalian">
+<!-- pengembalian-->
+<section class="page-section" id="pengembalian"> <!--section pengembalian-->
     <div class="container">
         <div class="text-center">
             <h2 class="section-heading text-uppercase">PENGEMBALIAN</h2>
@@ -15,16 +15,16 @@
     </div>
     <div class="row">
         <div class="column">
-            <img src="assets/img/miniV.png" alt="" style="width:350px;height:210px;">
+            <img src="assets/img/miniV.png" alt="" style="width:350px;height:210px;"><!-- menampilkan image/gambar dari mobil yang disewa-->
         </div>
-        <div class="column middle">
-            <p style="font-size:130%;" style="text-align:left;"> Nama Mobil : Mini Cooper Convertible.</p>
+        <div class="column middle"> <!-- mendeskripsikan nama mobil dan tengat waktu sesuai dengan peminjaman sewa mobil oleh user-->
+            <p style="font-size:130%;" style="text-align:left;"> Nama Mobil : Mini Cooper Convertible.</p> 
             <p style="font-size:130%;" style="text-align:left;"> Tengat Waktu : 04/04/2021</p>
         </div>
         <div class="column right">
-            <a class="btn btn-primary" href="#submited">Konfirmasi Pengembalian</a>
+            <a class="btn btn-primary" href="#submited">Konfirmasi Pengembalian</a><!--berisi button untuk mengkonfirmasi pengembalian-->
         </div>
-        <p>* Silahkan lakukan segera konfirmasi pengembalian ketika tenggat waktu telah habis.<br>
+        <p>* Silahkan lakukan segera konfirmasi pengembalian ketika tenggat waktu telah habis.<br><!--berisi note untuk user-->
             Jika ingin menambahkan hari penyewaan silahkan konfirmasi ke Customer Service kami dalam kurung waktu 24 jam ketika tenggat waktu telah habis.<br>
             Jika dalam kurung waktu tersebut tidak ada konfirmasi dari penyewa. Maka akan kami lanjutkan ke pihak berwajib.</p>
     </div>
