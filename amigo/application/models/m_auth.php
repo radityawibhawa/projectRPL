710cf6849e16838ba20de45c97276ac462789cf7
Adinda Rahma Khumairah <60496594+Adinda18@users.noreply.github.com>
2021-05-25 15:32:31 +0700

    Update m_auth.php

--- amigo/application/models/m_auth.php
+++ amigo/application/models/m_auth.php
@@ -2,28 +2,30 @@
 
 class m_auth extends CI_model
 {
-
+    //Proses pengecekkan data dari pengguna yaitu email
     public function login($email)
     {
+        //pengecekkan email yang di inputkan oleh pengguna apakah sudah terdaftar dalam database atau tidak
         $pelanggan = $this->db->get_where('pelanggan', ['email' => $email])->row_array();
         if ($pelanggan) {
             // usernya ada
             return true;
         } else {
+            //usernya ga ada
             return false;
         }
     }
-
+    //mengambil semua data table pelanggan dari database
     public function getAllUser()
     {
         return $this->db->get('pelanggan')->result_array();
     }
-
+    //memasukkan data ke table pelanggan
     public function insertUser($data)
     {
         return $this->db->insert('pelanggan', $data);
     }
-
+    //fungsi untuk mengedit data user berdasarkan id pelanggan
     public function editUser($id_pelanggan)
     {
         $data = [
@@ -35,21 +37,21 @@ class m_auth extends CI_model
         $this->db->where('id_pelanggan', $id_pelanggan);
         $this->db->update('pelanggan', $data);
     }
-
+    //fungsi untuk menghapus data user berdasarkan id_pelanggan
     public function deleteUser($id_pelanggan)
     {
-        $this->db->where('id_pelanggan', $id_pelanggan); //database idpelanggan
+        $this->db->where('id_pelanggan', $id_pelanggan);
         $this->db->delete('pelanggan');
     }
-
+    //mengambil data user dari id_pelanggan
     public function getUserById($id_pelanggan)
     {
-        $this->db->where('id_pelanggan', $id_pelanggan); //database idpelanggan
+        $this->db->where('id_pelanggan', $id_pelanggan);
         return $this->db->get('pelanggan')->row_array();
     }
 
-    public function insertAppointment($data)
-    {
-        return $this->db->insert('appointment', $data); //meload database 
-    }
+    // public function insertAppointment($data)
+    // {
+    //     return $this->db->insert('appointment', $data);
+    // }
 }
