32c880f586f22969393f6694a968bc5b2f114c6c
rifaldositepu <62953043+rifaldositepu@users.noreply.github.com>
2021-05-25 15:17:26 +0700

    Update Sewa_mobil.php

--- amigo/application/controllers/Sewa_mobil.php
+++ amigo/application/controllers/Sewa_mobil.php
@@ -1,12 +1,12 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Sewa_mobil extends CI_Controller
+class Sewa_mobil extends CI_Controller // menamakan class controller
 {
 
-    function __construct()
+    function __construct() // mendefinisikan kebutuhan objek sebelum bisa digunakan
     {
-        parent::__construct();
+        parent::__construct();// pada baris ini, kita akan mempersiapkan kebutuhan 'parent' class terlebih dahulu, yaitu class sewa_mobil.
         $this->load->model('m_auth');
     }
 
@@ -17,10 +17,10 @@ class Sewa_mobil extends CI_Controller
         // } else if($this->session->userdata('role_id') == "2") {
         // 	redirect('pelanggan');
         // } else{
-        $data['title'] = 'Amigotics Rent - Beranda';
-        $this->load->view('pelanggan/header_pelanggan', $data);
-        $this->load->view('pelanggan/sewamobil');
-        $this->load->view('pelanggan/footer_pelanggan');
+        $data['title'] = 'Amigotics Rent - Beranda'; 
+        $this->load->view('pelanggan/header_pelanggan', $data); //meload views header_pelanggan
+        $this->load->view('pelanggan/sewamobil'); //meload views sewamobil
+        $this->load->view('pelanggan/footer_pelanggan'); //meload views footer_pelanggan
         // }
 
 
