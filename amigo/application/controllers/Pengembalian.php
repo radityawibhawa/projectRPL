17448d67a60f7cfd9ed9cf9d5f21073825c94ffd
rifaldositepu <62953043+rifaldositepu@users.noreply.github.com>
2021-05-25 15:17:56 +0700

    Update Pengembalian.php

--- amigo/application/controllers/Pengembalian.php
+++ amigo/application/controllers/Pengembalian.php
@@ -1,26 +1,26 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Pengembalian extends CI_Controller
+class Pengembalian extends CI_Controller //menamaan class controller
 {
 
-    function __construct()
+    function __construct() // mendefinisikan kebutuhan objek sebelum bisa digunakan
     {
-        parent::__construct();
-        $this->load->model('m_auth');
+        parent::__construct(); // pada baris ini, kita akan mempersiapkan kebutuhan 'parent' class terlebih dahulu, yaitu class pengembalian.
+        $this->load->model('m_auth'); //meload model
     }
 
-    public function index()
+    public function index() 
     {
         // if($this->session->userdata('role_id') == "1") {
         // 	redirect('admin');	
         // } else if($this->session->userdata('role_id') == "2") {
         // 	redirect('pelanggan');
         // } else{
-        $data['title'] = 'Amigotics Rent - Beranda';
-        $this->load->view('pelanggan/header_pelanggan', $data);
-        $this->load->view('pelanggan/pengembalian');
-        $this->load->view('pelanggan/footer_pelanggan');
+        $data['title'] = 'Amigotics Rent - Beranda'; 
+        $this->load->view('pelanggan/header_pelanggan', $data); //meload views header
+        $this->load->view('pelanggan/pengembalian'); //meload views pengembalian
+        $this->load->view('pelanggan/footer_pelanggan'); //meload views footer
         // }
 
 
