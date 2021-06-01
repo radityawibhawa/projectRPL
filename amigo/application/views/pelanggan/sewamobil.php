1bbd52ede68f72229fcef813f60e8bb52ef4ac71
Adinda Rahma Khumairah <60496594+Adinda18@users.noreply.github.com>
2021-06-01 14:56:02 +0700

    Update sewamobil.php

--- amigo/application/views/pelanggan/sewamobil.php
+++ amigo/application/views/pelanggan/sewamobil.php
@@ -1,9 +1,8 @@
 <script type='text/javascript'>
-    $(function() { //untuk menghubungkan project dengan jquery.js dan popper.js 
-        $('[data-toggle="tooltip"]').tooltip() //yang dimana bermaksud untuk memerintahkan tooltip() pada semua elemen yang memiliki atribute data-toggle="tooltip". 
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip()
     })
 </script>
-
 <!-- Masthead-->
 <header class="masthead">
     <div class="container">
@@ -14,78 +13,26 @@
 </header>
 
 <!-- daftar mobil-->
-<section class="page-section" id="daftarmobil"> <!-- merupakan page section dari daftar mobil-->
+<section class="page-section" id="daftarmobil">
     <div class="container">
         <div class="text-center">
             <h2 class="section-heading text-uppercase">DAFTAR MOBIL</h2>
         </div>
     </div>
     <div class="row">
-		<!-- Code yang akan digunakan ketika fungsi upload gambar telah berhasil
-		<?php foreach ($list_mobil as $mobil) : ?>
+        <?php foreach ($list_mobil as $mobil) : ?>
             <div class="column">
                 <img src="<?php echo base_url('assets/img/' . $mobil->gambar_mobil) ?>" width="340px" height="210px" />
                 <p></p>
                 <h5><?php echo $mobil->jenis_mobil ?></h5>
                 <p><?php echo $mobil->start_harga ?></p>
             </div>
-        <?php endforeach; ?> -->
-        <div class="column">
-            <img src="assets/img/alphard.jpg" alt="" style="width:340px;height:210px;"> <!-- memanggil image/gambar tipe mobil-->
-            <p></p>
-            <h5>Alphard</h5><!-- nama/jenis mobil-->
-            <p>Mulai dari Rp. 1.000.000</p> <!-- harga sewa mobil-->
-        </div>
-        <div class="column">
-            <img src="assets/img/yaris.webp" alt="" style="width:340px;height:230px;">
-            <h5>Toyota Yaris</h5>
-            <p>Mulai dari Rp. 500.000</p>
-        </div>
-        <div class="column">
-            <img src="assets/img/Accord.jpg" alt="" style="width:315px;height:210px;">
-            <p></p>
-            <h5>Honda Accord</h5>
-            <p>Mulai dari Rp. 500.000</p>
-        </div>
-        <div class="column">
-            <img src="assets/img/odyssey.png" alt="" style="width:300px;height:175px;">
-            <p></p>
-            <h5>Honda Odyssey</h5>
-            <p>Mulai dari Rp. 500.000</p>
-        </div>
-        <div class="column">
-            <img src="assets/img/hiace.jpg" alt="" style="width:330px;height:190px;">
-            <h5>Toyota HiAce</h5>
-            <p>Mulai dari Rp. 850.000</p>
-        </div>
-        <div class="column">
-            <img src="assets/img/miniV.png" alt="" style="width:340px;height:170px;">
-            <p></p>
-            <h5>Mini Cooper Convertible.</h5>
-            <p>Mulai dari Rp. 1.000.000</p>
-        </div>
-        <div class="column">
-            <img src="assets/img/honda fit.jpg" alt="" style="width:300px;height:165px;">
-            <p></p>
-            <h5>Honda Fit</h5>
-            <p>Mulai dari Rp. 500.000</p>
-        </div>
-        <div class="column">
-            <img src="assets/img/honda-xn.jpg" alt="" style="width:340px;height:180px;">
-            <h5>Honda XNV</h5>
-            <p>Mulai dari Rp. 850.000</p>
-        </div>
-        <div class="column">
-            <img src="assets/img/minip.png" alt="" style="width:340px;height:160px;">
-            <p></p>
-            <h5>Mini Cooper Countryman.</h5>
-            <p>Mulai dari Rp. 1.000.000</p>
-        </div>
+        <?php endforeach; ?>
     </div>
 
     <!-- halaman-->
-    <div class="column page"> <!-- merupakan column untuk membuat sebuah navigasi halaman -->
-        <a style="font-size:100%;" href="#1" target="_blank">1</a> 
+    <div class="column page">
+        <a style="font-size:100%;" href="#1" target="_blank">1</a>
         <a style="font-size:100%;" href="#2">2</a>
         <a style="font-size:100%;" href="#3">3</a><a>></a>
         <a style="font-size:100%;" href="#!">Halaman Selanjutnya</a>
