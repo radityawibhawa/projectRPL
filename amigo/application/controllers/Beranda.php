263c1554bed52d674fa9d8f235c360cbca67095f
antikaputripw <80316652+antikaputripw@users.noreply.github.com>
2021-06-15 13:42:21 +0700

    Update Beranda.php

--- amigo/application/controllers/Beranda.php
+++ amigo/application/controllers/Beranda.php
@@ -8,6 +8,7 @@ class Beranda extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->model('m_auth');
+		$this->load->model('m_pelanggan');
 	}
 
 	public function index()
@@ -18,12 +19,33 @@ class Beranda extends CI_Controller
 		// 	redirect('pelanggan');
 		// } else{
 //load folder vie dengan nama file header, beranda, footer
+		$email = $this->session->userdata('email');
+        $data['id'] = $this->m_auth->get_id_user_by_email($email);
+		$data['review'] = $this->m_pelanggan->get_data_review();
+		$data['mobil'] = $this->m_pelanggan->get_data_mobil();
 		$data['title'] = 'Amigotics Rent - Beranda';
 		$this->load->view('pelanggan/header_pelanggan', $data);
-		$this->load->view('pelanggan/beranda');
-		$this->load->view('pelanggan/footer_pelanggan');
+		$this->load->view('pelanggan/beranda',$data);
+		$this->load->view('pelanggan/footer_pelanggan',$data);
 		// }
-
-
 	}
+
+	public function sendMessage(){
+		$idUser = $this->uri->segment(3);
+		if($this->input->post('submit')){
+			if($this->m_pelanggan->sendMessage('$idUser') == TRUE){
+				$email = $this->session->userdata('email');
+		        $data['id'] = $this->m_auth->get_id_user_by_email($email);
+				$data['review'] = $this->m_pelanggan->get_data_review();
+				$data['success'] = 'MESSAGE SENT!!';
+				$data['review'] = $this->m_pelanggan->get_data_review();
+				$data['mobil'] = $this->m_pelanggan->get_data_mobil();
+				$data['title'] = 'Amigotics Rent - Beranda';
+				$this->load->view('pelanggan/header_pelanggan', $data);
+				$this->load->view('pelanggan/beranda',$data);
+				$this->load->view('pelanggan/footer_pelanggan');
+			}
+		}
+	}
+
 }
